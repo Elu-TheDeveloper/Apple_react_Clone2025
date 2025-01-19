9a6f8d1f927e980184a0fe2b60ea373018921265
unknown <eladaba2022@gmail.com>
2025-01-20 00:18:14 +0300

    loved

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Section1 from "./Components/Section One/Section1"
-import Section2 from "./Components/Section Two/Section2"
-import Section3 from "./Components/Section Three/Section3"
-import Section4 from "./Components/Section Four/Section4"
-import Section5 from "./Components/Section five/Section5"
-import Section6 from "./Components/Section Six/Section6"
+import Section1 from "./Components/Section-One/Section1.jsx"
+import Section2 from "./Components/Section-Two/Section2"
+import Section3 from "./Components/Section-Three/Section3.jsx"
+import Section4 from "./Components/Section-Four/Section4.jsx"
+import Section5 from "./Components/Section-five/Section5.jsx"
+import Section6 from "./Components/Section-Six/Section6.jsx"
 import Youtube from "./Components/Youtube/Youtube"
 import { Routes,Route } from "react-router-dom"
 import Mac from "./Components/Pages/Mac/Mac.jsx"
@@ -16,8 +16,8 @@ import Support from"./Components/Pages/Support/Support.jsx"
 import Tv from "./Components/Pages/TV/Tv.jsx"
 import "./assets/css/styles.css"
 import "./assets/css/bootstrap.css" 
-import FourOFour from "./Components/Pages/File not Found/FourOFour.jsx"
-import Commanlayout from "./Components/Commanlayout/Commanlayout.jsx"
+import FourOFour from "./Components/Pages/File-not-Found/FourOFour.jsx"
+import Commanlayout from "./Components/Comman-Layout/Commanlayout.jsx"
 import Iphones from "./Components/Product_and_Apple/Iphones.jsx"
 import Product_info from"./Components/Product_and_Apple/Product_info.jsx"
 
@@ -53,5 +53,4 @@ function App() {
       </Routes>
   );
 }
-
 export default App;
--- src/Components/Section-five/Section5.jsx
+++ src/Components/Section-five/Section5.jsx
@@ -1,39 +1,39 @@
 import watch from "../../assets/images/home/watch.png"
 function Section5() {
   return (
-    <section className="fifth-highlight-wrapper">
-		<div className="container-fluid">
-			<div className="row">
-				<div className="left-side-wrapper col-sm-12 col-md-6">
-					<div className="left-side-container right">
-					<div className="descrption-wrapper">
+    <section classNameName="fifth-highlight-wrapper">
+		<div classNameName="container-fluid">
+			<div classNameName="row">
+				<div classNameName="left-side-wrapper col-sm-12 col-md-6">
+					<div classNameName="left-side-container right">
+					<div classNameName="descrption-wrapper">
 							MacBook Pro
 							</div>
-							<div className="title-wrapper1">
+							<div classNameName="title-wrapper1">
 							A work of Smart
 							</div>
-						<div class="links-wrapper right">
+						<div className="links-wrapper right">
   <ul>
-    <li><a href="" class="primary-btn">Learn more</a></li>
-    <li><a href="" class="secondary-btn">Buy</a></li>
+    <li><a href="" className="primary-btn">Learn more</a></li>
+    <li><a href="" className="secondary-btn">Buy</a></li>
   </ul>
 </div>
 					</div>
 				</div>
-				<div className="right-side-wrapper col-sm-12 col-md-6">
-					<div className="right-side-container">
-						<div className="top-logo-wrapper">
-							<div className="logo-wrapper">
+				<div classNameName="right-side-wrapper col-sm-12 col-md-6">
+					<div classNameName="right-side-container">
+						<div classNameName="top-logo-wrapper">
+							<div classNameName="logo-wrapper">
 								<img src={watch}/>
 							</div>
 						</div>
-						<div className="description-wrapper">
+						<div classNameName="description-wrapper">
 							New Finish. Never quit.
 						</div>
-						<div class="links-wrapper">
+						<div className="links-wrapper">
   <ul>
-    <li><a href="" class="primary-btn">Learn more</a></li>
-    <li class="button"><a href="" class="secondary-btn">Buy</a></li>
+    <li><a href="" className="primary-btn">Learn more</a></li>
+    <li className="button"><a href="" className="secondary-btn">Buy</a></li>
   </ul>
 </div>
 					</div>
